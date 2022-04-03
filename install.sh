8bfd8f19ab9b01da78dd25963a3048f5470c1750
xiaoyu2006 <caoyi06@qq.com>
2022-04-03 09:39:04 +0000

    Fix permission error caused by Docker

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ docker build . -t mcts-runtime
 echo =================DOWNLOADING MINECRAFT==================
 cd server
 chmod +x start.sh
-docker run --mount type=bind,source="$(pwd)",target=/workspace --rm mcts-runtime /dragonwell/bin/java -jar fabric-installer-0.10.2.jar server mcversion 1.18.2 -downloadMinecraft
+docker run --mount type=bind,source="$(pwd)",target=/workspace -u $(id -u):$(id -g) --rm mcts-runtime /dragonwell/bin/java -jar fabric-installer-0.10.2.jar server mcversion 1.18.2 -downloadMinecraft
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm --name minecraft -dit --network host --mount type=bind,source="$(pwd)",target=/workspace mcts-runtime
+docker run --rm --name minecraft -dit --network host --mount type=bind,source="$(pwd)",target=/workspace -u $(id -u):$(id -g) mcts-runtime
